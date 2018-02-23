053ec8003a21ecddd79385b28e567b8ea734d185
Peter Poliwoda <peterpoliwoda@ie.ibm.com>
2018-02-23 14:17:14 +0000

    Random characters file script

---
+++ generate-random-char-file.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const charLimit = process.argv[2] || 18588430;
+const fileName = process.argv[3] || '18mbs.please';
+const helpParams = ['help', '-h', '--help', '-help'];
+
+// print process.argv
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+});
+
+function generateRandomChars() {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < charLimit; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+function generateRandomCharFile() {
+    let fileContent = generateRandomChars();
+    fs.writeFile(fileName, fileContent);
+}
+
+function showHelp() {
+    console.log('### RANDOM CHAR FILE GENERATOR ###');
+    console.log('\n');
+    console.log('Usage: ');
+    console.log('$ node generate-random-char-file.js [charNumberLimit] [fileName]');
+    console.log('$ ...  --help     Show this help');
+    console.log('\n');
+}
+
+if (process.argv[2] && ~helpParams.indexOf(process.argv[2])) {
+    showHelp();
+} else {
+    generateRandomCharFile();
+    console.log('Generated file: ' + fileName + ' with ' + charLimit + ' characters.');
+}
